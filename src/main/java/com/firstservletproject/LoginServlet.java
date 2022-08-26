3f093c8706ed46657588966868083f6f645f96dc
Dipali <dipssonawane123@gmail.com>
2022-08-26 23:14:54 +0530

    Modified:valid UserName feature is modified

--- src/main/java/com/firstservletproject/LoginServlet.java
+++ src/main/java/com/firstservletproject/LoginServlet.java
@@ -1,7 +1,4 @@
 package com.firstservletproject;
-import java.io.IOException;
-import java.io.PrintWriter;
-
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebInitParam;
@@ -9,27 +6,35 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 
-@WebServlet(description = "Login Servlet Testing", urlPatterns = { "/LoginServlet" }, initParams = {
-        @WebInitParam(name = "user", value = "Dipali"), @WebInitParam(name = "password", value = "12345") })
+@WebServlet(
+        description = "Login Servlet Testing",
+        urlPatterns = {"/LoginServlet"},
+        initParams = {
+                @WebInitParam(name = "user", value = "Dipali"),
+                @WebInitParam(name = "password", value="12345")
+        }
+)
 public class LoginServlet extends HttpServlet {
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String user= request.getParameter("user");
+        String pwd= request.getParameter("pwd");
 
-        String user = request.getParameter("user");
+        String userID=getServletConfig().getInitParameter( "user");
+        String password=getServletConfig().getInitParameter("password");
 
-        String pwd = request.getParameter("pwd");
-        String userId = getServletConfig().getInitParameter("user");
-        String password = getServletConfig().getInitParameter("password");
-        if (userId.equals(user) && password.equals(pwd)) {
-            request.setAttribute("user", user);
-            request.getRequestDispatcher("LoginSuccess.jsp").forward(request, response);
+        String nameValidate = "^[A-Z]{1}[a-z]{2,}";
+        if(userID.equals(user) && userID.matches(nameValidate) && password.equals(pwd)) {
+            request.setAttribute( "user", user);
+            request.getRequestDispatcher("LoginSuccess.jsp").forward (request,response);
         } else {
             RequestDispatcher rd = getServletContext().getRequestDispatcher("/Login.html");
-            PrintWriter out = response.getWriter();
-            out.println("<font colour=red> Either User name or Password is Wrong</font>");
-            rd.include(request, response);
+            PrintWriter out= response.getWriter();
+            out.println("<font color=red>Either user name or password is wrong.</font>");
+            rd.include(request,response);
         }
     }
 }
